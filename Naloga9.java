0e6dd5f4ebeaf72b79ab3abaa3539609bfeb69df
JakobSk <jakob.novopecenmv@gmail.com>
2018-12-26 09:37:39 +0100

    finished

--- Naloga9.java
+++ Naloga9.java
@@ -1,7 +1,89 @@
 import java.io.*;
+import java.util.*;
+
+//java Naloga9 C:\Users\User\Desktop\test.txt C:\Users\User\Desktop\out.txt
 
 class Struct {
 
+    Node[] lista;
+
+    class Node {
+
+        int id;
+        Set<Integer> prijatelji;
+
+        Node(int id, Set<Integer> prijatelji) {
+
+            this.id = id;
+            this.prijatelji = prijatelji;
+        }
+    }
+
+    public void init(int st) {
+
+        lista = new Node[st * 2 + 1];
+    }
+
+    public void work(String data, PrintWriter izhod) {
+
+        String[] d = data.split(",");
+        int a = Integer.parseInt(d[0]);
+        int b = Integer.parseInt(d[1]);
+
+
+        if (lista[a] == null) {
+
+            Node an = new Node(a, null);
+            an.prijatelji = new HashSet<>();
+            lista[a] = an;
+            an.prijatelji.add(a);
+        }
+
+        if (lista[b] == null){
+
+            Node bn = new Node(b, null);
+            bn.prijatelji = new HashSet<>();
+            lista[b] = bn;
+            bn.prijatelji.add(b);
+        }
+
+        if (add(a, b))
+            izhod.printf("%S\n", data);
+    }
+
+    public boolean add (int a, int b) {
+
+        if (lista[a].prijatelji.contains(b))
+            return true;
+
+        lista[a].prijatelji.addAll(lista[b].prijatelji);
+
+        for (Node x : lista) {
+
+            if (x != null)
+                x.prijatelji.addAll(lista[a].prijatelji);
+        }
+
+        return false;
+    }
+
+    public void izpisi () {
+
+        for (Node i : lista){
+
+            if (i != null) {
+
+                System.out.printf("ID: %d\n", i.id);
+
+                for (int j : i.prijatelji) {
+
+                    System.out.printf("%d, ", j);
+                }
+                System.out.println();
+            }
+        }
+        System.out.println("_____________________");
+    }
 }
 
 public class Naloga9 {
@@ -16,15 +98,18 @@ public class Naloga9 {
                     new FileReader(args[0])
             );
 
-
-
             PrintWriter izhod = new PrintWriter(
                     new FileWriter(args[1])
             );
 
             Struct struct = new Struct();
 
+            int stevilo = Integer.parseInt(vhod.readLine());
 
+            struct.init(stevilo);
+
+            for (int i = 0; i < stevilo; i++)
+                struct.work(vhod.readLine(), izhod);
 
             final long et = System.currentTimeMillis();
 
