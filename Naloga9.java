0d23ffa525979e293423ac4f5ca5859279b0dc82
JakobSk <jakob.novopecenmv@gmail.com>
2018-12-26 13:09:54 +0100

    added comments

--- Naloga9.java
+++ Naloga9.java
@@ -5,6 +5,9 @@ import java.util.*;
 
 class Struct {
 
+    //struct contains array of nodes
+    //each node represents a person and contains his id and a set of his friends
+
     Node[] lista;
 
     class Node {
@@ -21,15 +24,21 @@ class Struct {
 
     public void init(int st) {
 
+        //maximum amount of possible nodes out of N pairs is 2*N + 1 (+1 as id's start with 1, not with 0)
+
         lista = new Node[st * 2 + 1];
     }
 
     public void work(String data, PrintWriter izhod) {
 
+        //this function processes input "a,b"
+
         String[] d = data.split(",");
         int a = Integer.parseInt(d[0]);
         int b = Integer.parseInt(d[1]);
 
+        //if a node with id "a" or "b" doesn't exist, it's created here
+        //each person is also friend with himself, this ensures that when expanding someones circle of friends, everyone is added
 
         if (lista[a] == null) {
 
@@ -47,16 +56,25 @@ class Struct {
             bn.prijatelji.add(b);
         }
 
+        //friendship between a and b is added here, add(a,b) returns true if such friendship is already present
+        //useless instruction is then printed
+
         if (add(a, b))
             izhod.printf("%S\n", data);
     }
 
     public boolean add (int a, int b) {
 
+        //if a and b are already friends, return true
+
         if (lista[a].prijatelji.contains(b))
             return true;
 
+        //add all b's friends to a
+
         lista[a].prijatelji.addAll(lista[b].prijatelji);
+
+        //expand the circle of friends to all people that are in a's circle of friends
 
         for (int iterate : lista[a].prijatelji)
             lista[iterate].prijatelji.addAll(lista[a].prijatelji);
